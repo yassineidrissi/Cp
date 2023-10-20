c5cb6622ef6ba21292ef5851fa0fb1e83605c0df
Yassine Idrissi <yaidriss@c1r4p1.1337.ma>
2023-10-20 21:55:40 +0100

    add all modifications

--- Cpp01/ex06/Harl.hpp
+++ Cpp01/ex06/Harl.hpp
@@ -5,8 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/20 21:31:41 by yaidriss          #+#    #+#             */
-/*   Updated: 2023/10/20 21:31:42 by yaidriss         ###   ########.fr       */
+/*   Created: 2023/10/19 18:11:27 by yaidriss          #+#    #+#             */
+/*   Updated: 2023/10/20 20:18:11 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+
+typedef void (*func)(void); 
+
+class Harl{
+	public:
+		Harl();
+		~Harl();
+		void complain(std::string level);
+	private:
+		std::string level;
+};
--- Cpp01/ex06/main.cpp
+++ Cpp01/ex06/main.cpp
@@ -5,9 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/20 21:30:42 by yaidriss          #+#    #+#             */
-/*   Updated: 2023/10/20 21:30:50 by yaidriss         ###   ########.fr       */
+/*   Created: 2023/10/19 18:11:05 by yaidriss          #+#    #+#             */
+/*   Updated: 2023/10/20 20:16:40 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include 
+#include "Harl.hpp"
+
+int main()
+{
+	Harl harl;
+	std::cout << "wish level is u want ?:" << std::endl;
+	std::string level;
+	std::getline(std::cin, level);
+	harl.complain(level);
+	return 0;
+}
